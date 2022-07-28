606ed19bbf8f15d4b9c9e71712c8feb2ddf52503
sergiozamir <sergiozamir@gmail.com>
2022-07-27 20:27:33 -0500

    primer practica

---
+++ main.js
@@ -0,0 +1,60 @@
+//alert("hola mundo");
+
+
+// no se usa var//
+/*var nombre;
+nombre = "sergio zamir";
+console.log(nombre);'
+
+let nombre = "sergio zamir"; 
+let edad = 37;
+
+console.log(nombre);
+console.log(edad);*/
+
+/*ejemplo prompt
+
+let nombre = "";
+let edad = 0;
+
+nombre = prompt("ingrese su nombre");
+edad = prompt("ingrese su edad");
+
+console.log("tu nombre es " + nombre + " y tu edad es " + edad);*/
+
+/*if y else*/
+
+/*let nombre = "guille";
+let apellido = "guille";
+let edad = 40;
+
+if ( 1 == 1) {
+    console.log("1 es igual a 1");
+    console.log("que loco 1 es igual a 1");
+    console.log("increíble 1 es igual a 1");
+} else {
+    console.log("otra cosa");
+}
+
+console.log("fin de codigo");*/
+
+
+/*ejemplos clase*/
+
+/*let tabla = 3 ;
+    for(let i = 1; i <=10; i++){
+        tabla = tabla *i
+    console.log(tabla);
+
+    }*/
+
+   /*let numero = 0;
+for(let i= 0; i<= 5; i++){
+    console.log(numero + i)
+}*/
+
+
+promt para pedir
+for o while para bucle o ciclo
+if o swtich para condiciones
+console log o alert para dar la salida
